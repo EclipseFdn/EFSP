2f5b60fa33e28cf41eda149fb87ccf91c7d3fb06
Wayne Beaton <wayne.beaton@eclipse-foundation.org>
2021-07-13 11:53:11 -0400

    Add a script for generating the diff.
    Generate a diff between the 1.2 and draft 1.3 versions of the EFSP. Note
    that this script likely only runs on Wayne's desktop. See comments in
    the diff.sh file.

--- releng/diff.sh
+++ releng/diff.sh
@@ -1,3 +1,27 @@
-wget "https://www.eclipse.org/projects/efsp/content/efsp_v1.2.html" -O - \
-> /tmp/old.html
-node /gitroot/temp/htmldiff.js/htmldiff-cli.js /tmp/old.html target/generated-docs/efsp.html /gitroot/www.eclipse.org/projects/efsp/content/diff_v1.3.html
+#!/bin/bash
+# Copyright (C) Eclipse Foundation, Inc. and others. 
+# 
+# This program and the accompanying materials are made available under the
+# terms of the Eclipse Public License v. 2.0 which is available at
+# http://www.eclipse.org/legal/epl-2.0.
+# 
+# SPDX-License-Identifier: EPL-2.0
+
+# This is a quick and dirty utility for generating a diff between the v1.2
+# version of the EFSP and the version that is under development and moving
+# that diff into the www.eclipse.org/projects directory (where it can be
+# committed to update the website). That is, for this script to be of any
+# value, the www.eclipse.org/projects Git repository must be cloned into
+# /gitroot/www.eclipse.org/projects on the local file system.
+# 
+# Further, this script should be run after the Maven build script is run
+# to produce an update to the new version of the EFSP.
+#
+# TODO incorporate this script into the Maven build.
+# 
+# This script likely only runs on Wayne's desktop.
+
+cd "${BASH_SOURCE%/*}"
+npm i node-htmldiff
+wget "https://www.eclipse.org/projects/efsp/content/efsp_v1.2.html" -O - > /tmp/old.html
+node ./node_modules/node-htmldiff/htmldiff-cli.js /tmp/old.html ../target/generated-docs/efsp.html /gitroot/www.eclipse.org/projects/efsp/content/diff_v1.3.html
